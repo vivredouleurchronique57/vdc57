0d7e854d0e8aba16725f03203ed5e7175ec7e0e3
Philippe BARRE <philippe.barre.57@gmail.com>
2023-06-10 00:14:02 +0200

    feat: install flowbite feat: init colors wip: add navbar

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav class="scrim-bg fixed z-40 top-0 inset-x-0 pt-3 px-3" aria-label="Main Menu">
+  <!-- <nav class="scrim-bg fixed z-40 top-0 inset-x-0 pt-3 px-3" aria-label="Main Menu">
     <ul class="flex">
       <li class="flex-1">
-        <nuxt-link class="btn block" to="/">Home</nuxt-link>
+        <nuxt-link class="btn block" to="/">Homee</nuxt-link>
       </li>
       <li class="flex-1 ml-2">
         <nuxt-link class="btn block" to="/blog">Blog</nuxt-link>
@@ -11,12 +11,67 @@
         <nuxt-link class="btn block" to="/projects">Projects</nuxt-link>
       </li>
     </ul>
+  </nav> -->
+
+  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="https://flowbite.com/" class="flex items-center">
+        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      </a>
+      <button
+        data-collapse-toggle="navbar-default"
+        type="button"
+        class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-default"
+        aria-expanded="false"
+      >
+        <span class="sr-only">Open main menu</span>
+        <svg
+          class="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul
+          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+        >
+          <li>
+            <nuxt-link
+              to="/"
+              class="nav-item text-white bg-blue-700 md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+              aria-current="page"
+              >Accueil</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link
+              to="/blog"
+              class="nav-item text-gray-900  hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >Blog</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link to="/projects" class="nav-item">Projects</nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
 }
 </script>
 
@@ -55,11 +110,23 @@ export default {
 
 /* Need two because of smoother switching between color modes */
 @keyframes fadeIn1 {
-  from { opacity: 0; }
-  to { opacity: 1; }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 @keyframes fadeIn2 {
-  from { opacity: 0; }
-  to { opacity: 1; }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+.nav-item {
+  @apply block py-2 pl-3 pr-4 rounded;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,50 +10,69 @@ module.exports = {
     darkMode: 'class',
     extend: {
       fontFamily: {
-        sans: ['Inter']
+        sans: ['Inter'],
       },
       colors: {
         gray: {
-          '50': 'var(--color-gray-50)', // #F9FAFB
-          '100': 'var(--color-gray-100)', // #F3F4F6
-          '200': 'var(--color-gray-200)', // #E5E7EB
-          '300': 'var(--color-gray-300)', // #D1D5DB
-          '400': 'var(--color-gray-400)', // #9CA3AF
+          50: 'var(--color-gray-50)', // #F9FAFB
+          100: 'var(--color-gray-100)', // #F3F4F6
+          200: 'var(--color-gray-200)', // #E5E7EB
+          300: 'var(--color-gray-300)', // #D1D5DB
+          400: 'var(--color-gray-400)', // #9CA3AF
           DEFAULT: 'var(--color-gray)', // #6B7280
-          '600': 'var(--color-gray-600)', // #4B5563
-          '700': 'var(--color-gray-700)', // #374151
-          '800': 'var(--color-gray-800)', // #1F2937
-          '900': 'var(--color-gray-900)' // #111827
+          600: 'var(--color-gray-600)', // #4B5563
+          700: 'var(--color-gray-700)', // #374151
+          800: 'var(--color-gray-800)', // #1F2937
+          900: 'var(--color-gray-900)', // #111827
         },
         primary: {
-          '100': 'var(--color-primary-100)', // #ECEFF5
-          '200': 'var(--color-primary-200)', // #C3CBDC
-          '300': 'var(--color-primary-300)', // #9BA8C2
-          '400': 'var(--color-primary-400)', // #7686A6
-          DEFAULT: 'var(--color-primary)', // #526488
-          '600': 'var(--color-primary-600)', // #3E4F70
-          '700': 'var(--color-primary-700)', // #2C3A57
-          '800': 'var(--color-primary-800)', // #1C263B
-          '900': 'var(--color-primary-900)' // #0D131E
-        }
+          50: 'var(--color-primary-50)',
+          100: 'var(--color-primary-100)',
+          200: 'var(--color-primary-200)',
+          300: 'var(--color-primary-300)',
+          400: 'var(--color-primary-400)',
+          DEFAULT: 'var(--color-primary)',
+          600: 'var(--color-primary-600)',
+          700: 'var(--color-primary-700)',
+          800: 'var(--color-primary-800)',
+          900: 'var(--color-primary-900)',
+        },
+        secondary: {
+          50: 'var(--color-secondary-50)',
+          100: 'var(--color-secondary-100)',
+          200: 'var(--color-secondary-200)',
+          300: 'var(--color-secondary-300)',
+          400: 'var(--color-secondary-400)',
+          DEFAULT: 'var(--color-secondary)',
+          600: 'var(--color-secondary-600)',
+          700: 'var(--color-secondary-700)',
+          800: 'var(--color-secondary-800)',
+          900: 'var(--color-secondary-900)',
+        },
       },
       screens: {
-        xs: '480px' // Added xs
+        xs: '480px', // Added xs
         // 'sm': '640px',
         // 'md': '768px',
         // 'lg': '1024px',
         // 'xl': '1280px',
-      }
-    }
+      },
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  content: [
+    // other files...
+    "./node_modules/flowbite.{js,ts}"
+  ],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
     content: ['components/**/*.vue', 'layouts/**/*.vue', 'pages/**/*.vue', 'plugins/**/*.js', 'nuxt.config.js'],
     options: {
       // Set safelist in nuxt.config.js -> purgeCSS.safelist: ['btn', 'icon']
-    }
-  }
+    },
+  },
 }
